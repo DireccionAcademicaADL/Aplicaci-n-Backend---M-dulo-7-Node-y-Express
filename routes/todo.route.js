3c0bb530ab5c47567e046a0ad2f0a989050677be
Javier <jarratia34@gmail.com>
2023-11-30 16:25:13 -0300

    Incorporación de sistema de autorización y restricción con JWT

--- routes/todo.route.js
+++ routes/todo.route.js
@@ -1,22 +1,23 @@
 import { todoController } from "../controllers/todo.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 import { Router } from "express";
 
 const router = Router();
 
 // GET /todos
-router.get("/", todoController.read);
+router.get("/", authMiddleware, todoController.read);
 
 // GET /todos/:id
-router.get("/:id", todoController.readById);
+router.get("/:id", authMiddleware, todoController.readById);
 
 // POST /todos
-router.post("/", todoController.create);
+router.post("/", authMiddleware, todoController.create);
 
 // PUT /todos/:id
-router.put("/:id", todoController.update);
+router.put("/:id", authMiddleware, todoController.update);
 
 // DELETE /todos/:id
-router.delete("/:id", todoController.remove);
+router.delete("/:id", authMiddleware, todoController.remove);
 
 export default router;
